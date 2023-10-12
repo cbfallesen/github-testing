5b3fe1deb4fc5b7a9fdcddc3a88f4b054f73672f
Caroline Fallesen <carolinefallesen@gmail.com>
2023-10-12 13:04:56 +0200

    Initial commit

---
+++ github-testing/Program.cs
@@ -0,0 +1,5 @@
+﻿using Octokit;
+
+var github = new GitHubClient(new ProductHeaderValue("github-testing"));
+var user = await github.User.Get("cbfallesen");
+Console.WriteLine(user.Followers + " folks love the half ogre!");
